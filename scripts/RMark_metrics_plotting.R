28cc32d3d02d5a75ebf4c7173fde96274254419b
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2018-07-08 16:04:29 -0400

    updated plotting script

--- scripts/RMark_metrics_plotting.R
+++ scripts/RMark_metrics_plotting.R
@@ -1,6 +1,6 @@
 # plotting RMark results
 
-rmark_results <- read.csv("data/MARKdata/MARKoutput_PP_all_S.stratum.p.dot.Psi.s.csv", stringsAsFactors = FALSE)
+rmark_results <- read.csv("data/top_model_summary.csv", stringsAsFactors = FALSE)
 
 rmark_results$time = c("pre", "post", "pre", "post", "pre", "post", NA, 
                        "pre", "post", "pre", "post", "pre", "post",
@@ -19,12 +19,15 @@ library(tidyverse)
 plot_rmark <- rmark_results %>% 
   filter(metric != "p", stratum == "A" | stratum == "B" | stratum == "AB" | stratum == "BA")
 
+plot_rmark$time <- factor(plot_rmark$time, levels = c("pre", "post"))
 ggplot(plot_rmark[(plot_rmark$metric == "S"),]) +
   geom_pointrange(aes(x = time, y = estimate, 
                       ymin = (estimate - se), ymax = (estimate + se), 
                       color = stratum), 
                   position = position_dodge(.1), size = .75, width = .2) +
+  ggtitle("Estimated Survival") +
   theme_classic()
+
 ggsave("figures/Survival.png")
 
 ggplot(plot_rmark[(plot_rmark$metric == "Psi"),]) +
@@ -32,5 +35,6 @@ ggplot(plot_rmark[(plot_rmark$metric == "Psi"),]) +
                       ymin = (estimate - se), ymax = (estimate + se), 
                       color = stratum), 
                   position = position_dodge(.1), size = .75, width = .2) +
+  ggtitle("Transition Probability") + 
   theme_classic()
 ggsave("figures/Psi.png")
