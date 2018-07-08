23032dd21ae8fa589c53663a1665d7285ef1d14e
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2018-07-08 19:08:57 -0400

    RMark updated plots

--- scripts/RMark_metrics_plotting.R
+++ scripts/RMark_metrics_plotting.R
@@ -4,14 +4,16 @@
 
 # LIBRARIES and DATA #
 library(tidyverse)
+library(patchwork)
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 rmark_results <- read.csv("data/top_model_summary.csv", stringsAsFactors = FALSE)
 
 # DATA PREP #
-rmark_results$time = c("pre", "post", "pre", "post", "pre", "post", NA, 
-                       "pre", "post", "pre", "post", "pre", "post",
-                       "pre", "post", "pre", "post", "pre", "post")
+rmark_results$time = c("Before", "After", "Before", "After", "Before", "After", NA, 
+                       "Before", "After", "Before", "After", "Before", "After",
+                       "Before", "After", "Before", "After", "Before", "After")
+plot_rmark$time <- factor(plot_rmark$time, levels = c("Before", "After"))
 
 rmark_results$metric = rep("S", nrow(rmark_results))
 rmark_results$metric[7] = "p"
@@ -30,23 +32,40 @@ plot_rmark <- rmark_results %>%
 
 # PLOTTING #
 
-plot_rmark$time <- factor(plot_rmark$time, levels = c("pre", "post"))
-ggplot(plot_rmark[(plot_rmark$metric == "S"),], color = treatment) +
+x_axis_title <- expression(paste(bolditalic("C. baileyi"), bold(" Infiltration Status")))
+
+plot1 <- ggplot(plot_rmark[(plot_rmark$metric == "S"),], color = Treatment) +
   geom_pointrange(aes(x = time, y = estimate, 
                       ymin = (estimate - se), ymax = (estimate + se), 
-                      color = Treatment), 
-                  position = position_dodge(.1), size = .75, width = .2) +
+                      color = Treatment, shape = Treatment), 
+                      position = position_dodge(.1), size = 1, width = .2) +
   scale_colour_manual(values = cbPalette) + 
-  ggtitle("Estimated Survival") + # put title in the middle
-  theme_classic()
+  xlab(x_axis_title) +
+  ylab("Estimated Survival") +
+  theme_classic() +
+  theme(panel.border = element_rect(fill = NA, colour = "black", size = 1),
+        axis.title.x = element_text(face = "bold", size = 14, margin = margin(t = 10)),
+        axis.title.y = element_text(face = "bold", size = 14, margin = margin(r = 10)),
+        axis.text.x = element_text(face = "bold", size = 12),
+        axis.text.y = element_text(face = "bold", size = 12))
 
 ggsave("figures/Survival.png")
 
-ggplot(plot_rmark[(plot_rmark$metric == "Psi"),]) +
+plot2 <- ggplot(plot_rmark[(plot_rmark$metric == "Psi"),]) +
   geom_pointrange(aes(x = time, y = estimate, 
                       ymin = (estimate - se), ymax = (estimate + se), 
-                      color = stratum), 
-                  position = position_dodge(.1), size = .75, width = .2) +
-  ggtitle("Transition Probability") + 
-  theme_classic()
+                      color = Treatment, shape = Treatment), 
+                  position = position_dodge(.1), size = 1, width = .2) +
+  scale_colour_manual(values = cbPalette) + 
+  xlab(x_axis_title) +
+  ylab("Transition Probability") +
+  theme_classic() +
+  theme(panel.border = element_rect(fill = NA, colour = "black", size = 1),
+        axis.title.x = element_text(face = "bold", size = 14, margin = margin(t = 10)),
+        axis.title.y = element_text(face = "bold", size = 14, margin = margin(r = 10)),
+        axis.text.x = element_text(face = "bold", size = 12),
+        axis.text.y = element_text(face = "bold", size = 12))
+
 ggsave("figures/Psi.png")
+plot3 <- plot1 + plot2
+ggsave("figures/S_and_Psi.png", plot3)
